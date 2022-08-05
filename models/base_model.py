08efc63a150ea79c3a898671736513a795829bc7
Peacebern <peace8282bern@gmail.com>
2022-08-05 15:17:48 +0000

    m

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,32 @@
-#!/usr/bin/pyhon3
-"""
-Parent class that will inherit
-"""
-import uuid
-from datetime import datetime
+#!/usr/bin/python3
 
+import uuid
+import datetime
 
 class BaseModel:
-    """Defines all common attributes/methods
-    """
-    def __init__(self, *args, **kwargs):
-        """initializes all attributes
-        """
-        self.name = "Hello"
-        self.my_number = "45"
+    def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at =  datetime.datetime.now()
+
 
     def __str__(self):
-        """returns class name, id and attribute dictionary
-        """
-        class_name = "[" + self.__class__.__name__ + "]"
-        return class_name + " (" + self.id + ") " + str(self.__dict__)
-
+        """ the string representation of the str object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                    self.id,
+                                    self.__dict__)
     def save(self):
-        """updates last update time
         """
-        self.updated_at = datetime.now()
+        updating the public instance attribute updated_at with the current 
+        datetime
+        """
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """creates a new dictionary, adding a key and returning
-        datemtimes converted to strings
-        """
-        new_dict = {}
+        result_dict = self.__dict__
+        result_dict["__class__"] = self.__class__.__name__
+        result_dict["created_at"] = strftime(%Y-%m-%dT%H:%M:%S.%f)
+        result_dict["updated_at"] = strftime(%Y-%m-%dT%H:%M:%S.%f)
 
-        for key, values in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
-            else:
-                new_dict[key] = values
-        new_dict['__class__'] = self.__class__.__name__
-
-        return new_dict
+        return result_dict
 

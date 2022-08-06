bc655c19496746266ff4b88ddc2959abe5383354
Gejix <geraldjuwah@gmail.com>
2022-08-07 00:49:26 +0100

    [fix]:Updating attribute

--- models/base_model.py
+++ models/base_model.py
@@ -15,25 +15,24 @@ class BaseModel:
         """initializes all attributes
         """
         if not kwargs:
-            self.name = ""
-            self.my_number = 0
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
         else:
-            self.name = kwargs['name']
-            self.my_number = kwargs['my_number']
-            self.id = kwargs['id']
             f = "%Y-%m-%dT%H:%M:%S.%f"
-            self.created_at = datetime.strptime(kwargs['created_at'], f)
-            self.updated_at = datetime.strptime(kwargs['updated_at'], f)
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """returns class name, id and attribute dictionary
         """
         class_name = "[" + self.__class__.__name__ + "]"
-        return class_name + " (" + self.id + ") " + str(self.__dict__)
+        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(dct)
 
     def save(self):
         """updates last update time
@@ -51,7 +50,10 @@ class BaseModel:
             if key == "created_at" or key == "updated_at":
                 new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
             else:
-                new_dict[key] = values
+                if not values:
+                    pass
+                else:
+                    new_dict[key] = values
         new_dict['__class__'] = self.__class__.__name__
 
         return new_dict
